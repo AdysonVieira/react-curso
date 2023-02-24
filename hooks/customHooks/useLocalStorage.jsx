f5d93c94af66e1c7e7a5cebda28d5bc54d5b0bd1
Adyson Vieira <contato.adyson@gmail.com>
2023-02-24 12:02:12 -0300

    corrigindo useLocalStorage

--- hooks/customHooks/useLocalStorage.jsx
+++ hooks/customHooks/useLocalStorage.jsx
@@ -1,14 +1,14 @@
 // useLocalStorage #############
 // Recebe uma chave e um valor incial, busca no localStorage se há algum item e seta na state, quando a state é modificada o novo valor é armazenado no localStorage
 const useLocalStorage = (key, inicial = '') => {
-    const [state, setState] = React.useState(() => {
-        const item = window.itemStorage.getItem(key);
+    const [storedValue, setStoredValue] = React.useState(() => {
+        const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : inicial;
     });
     
     React.useEffect(() => {
-        window.localStorage.setItem(key, JSON.stringify(state));
-    }, [key, state]);
+        window.localStorage.setItem(key, JSON.stringify(storedValue));
+    }, [key, storedValue]);
 
-    return [state, setState]
+    return [storedValue, setStoredValue]
 }
